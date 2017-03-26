507b941e07b3ebb07aa9fb14aa000b9b99b31430
BUNCH <mattbunch@gmail.com>
2017-03-26 14:48:15 -0700

    Update

---
+++ www/js/constants.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+  angular.module('sprayApp.const',[])
+  .constant('sConst',{
+    apiUrl:'https://63tcrphej6.execute-api.us-east-1.amazonaws.com/prod/data'
+  })
+})();
